da883d6d67d6b191e6d52e39eb2b11c4441ec6d2
Max-Savik <alch.savik@gmail.com>
2025-06-16 15:45:42 +0200

    удалил дублирование анимации

--- script.js
+++ script.js
@@ -577,27 +577,6 @@ function show(view){
   navEarn   .classList.toggle('active', view === 'earn');
 }
 
-window.addEventListener('load', async () => {
-  // показываем контейнер
-  document.getElementById('lottieContainer').style.display = '';
-
-  // 1. Скачиваем оригинальный JSON
-  const res  = await fetch('https://nft.fragment.com/gift/bondedring-403.lottie.json');
-  const data = await res.json();
-
-  // 2. Отфильтровываем слои (например, убираем фон)
-  data.layers = data.layers.filter(layer => layer.nm !== 'Background');
-
-  // 3. Инициализируем анимацию уже с изменённым JSON
-  lottie.loadAnimation({
-    container: document.getElementById('lottieContainer'),
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    animationData: data
-  });
-});
-
 
 
 // =================== HISTORY BUTTON ===================
