7a42bc0f89f2503fe21ec4409cfcda2d66ad1f81
Max-Savik <alch.savik@gmail.com>
2025-07-03 00:58:56 +0200

    небольшие правки

--- server/server.js
+++ server/server.js
@@ -117,7 +117,7 @@ async function saveWithdrawals() {
   const tmp = WD_FILE + ".tmp";
   await fs.writeFile(tmp, JSON.stringify(withdrawals, null, 2));
   await fs.rename(tmp, WD_FILE); 
-
+}   
 const wallet = express.Router();
 wallet.use(userAuth);
 
