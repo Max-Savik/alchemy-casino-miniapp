c3b7f0abd87a853d64278f02b2162103d1f648bf
Max-Savik <alch.savik@gmail.com>
2025-07-21 04:10:38 +0200

    Update profile.js

--- docs/profile.js
+++ docs/profile.js
@@ -26,7 +26,6 @@ async function ensureJwt() {
   localStorage.setItem("jwt", jwtToken);
 }
 
-
 async function refreshBalance() {
   const r = await fetch(`${API_ORIGIN}/wallet/balance`, {
     credentials: "include",
